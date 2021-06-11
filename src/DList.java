a6581cf07a99cce83c5c9fcab93c06349f943c0a
Lahiru Lakshan <balahirulakshan@gmail.com>
2021-06-11 23:05:40 +0530

    Added the removeDuplicates method in D List

--- src/DList.java
+++ src/DList.java
@@ -11,4 +11,21 @@ public class DList {
             previous =null;
         }
     }
+    private static ListNode first,last;
+    /* function to remove duplicates from a
+    sorted doubly linked list */
+    public static void removeDuplicates() {
+        ListNode n = first;
+        while (n != null ){
+            while (n. next != null && n. next . data == n. data ){
+                // remove the next node from the list
+                n. next = n. next . next ;
+                if(n. next == null ) // n is now the last node
+                    last = n;
+                else
+                    n. next . previous = n;
+            }
+            n = n. next ;
+        }
+    }
 }
