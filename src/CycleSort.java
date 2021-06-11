21bb9e6d10ac74a55c4f9a38e1960cf1233bbcef
Lahiru Lakshan <balahirulakshan@gmail.com>
2021-06-11 23:01:18 +0530

    Updated CycleSort Algorithm file

--- src/CycleSort.java
+++ src/CycleSort.java
@@ -1,2 +1,27 @@
 public class CycleSort {
+
+    public static void sort (int [] values ){
+        boolean [] inPlace = new boolean [ values . length ]; // initially all false
+        for (int i=0; i< values . length ; )
+            if( inPlace [i])
+// this value is in the right place , move on
+                i++;
+            else {
+// determine where this value belongs
+                int p=0;
+                for(int j : values )
+                    if(j < values [i])
+                        p++;
+// swap entries at positions i and p
+                int temp = values [i];
+                values [i] = values [p];
+                values [p] = temp ;
+                inPlace [p] = true ;
+            }
+    }
+
+    public static void main(String[] args) {
+        int[] values = new int[]{4,3,5,7,9,1};
+        sort(values);
+        for (int j : values) System.out.print(j + " ");	}
 }
