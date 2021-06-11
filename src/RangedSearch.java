b7a4f4c053c0705a5004d1963426a390244ca537
Lahiru Lakshan <balahirulakshan@gmail.com>
2021-06-11 23:10:37 +0530

    Added the findInRange method in RangedSearch Algorithm

--- src/RangedSearch.java
+++ src/RangedSearch.java
@@ -1,2 +1,17 @@
 public class RangedSearch {
+    public static int findInRange (int [] A, int low , int high ){
+        int first =0, last =A.length , middle =A.length /2;
+        while (first <= last ){
+            if(A[ middle ]<low )
+                last =middle -1;
+            else if(A[ middle ]> high )
+                first = middle +1;
+            else
+                return middle ;
+            middle =( first + last )/2;
+        }
+        return -1;
+    }
+
+
 }
