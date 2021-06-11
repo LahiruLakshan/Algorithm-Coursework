7114bfe431592d779490b0c5526c6efd31e5adc0
Lahiru Lakshan <balahirulakshan@gmail.com>
2021-06-11 23:04:00 +0530

    Created List Node Class in D List

--- src/DList.java
+++ src/DList.java
@@ -1,2 +1,14 @@
 public class DList {
+    public static class ListNode
+    {
+        int data;
+        ListNode next, previous;
+
+        ListNode(int data)
+        {
+            this.data=data;
+            next=null;
+            previous =null;
+        }
+    }
 }
